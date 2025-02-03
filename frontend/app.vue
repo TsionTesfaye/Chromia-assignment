ffada1ce2169c651f48f07799c2bc9446c370040
TsionTesfaye <tsitesfaye87466@gmail.com>
2025-02-04 00:21:16 +0300

    full

---
+++ frontend/app.vue
@@ -0,0 +1,18 @@
+<!-- ~/App.vue -->
+<script setup lang="ts">
+import SessionProvider from "~/components/sessionProvider.vue" 
+import AppInitializer from "~/components/appInitializer.vue" 
+import Toaster from "./components/ui/toast/Toaster.vue" 
+
+
+</script>
+
+<template>
+  <SessionProvider>
+    <AppInitializer />
+    <Toaster />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </SessionProvider>
+</template>
